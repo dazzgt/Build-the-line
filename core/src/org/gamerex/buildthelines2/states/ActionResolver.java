0186a5f6ad725ab7d8d35aaed276b33c3b9910dc
Redyukov Pavel <dazzgt@gmail.com>
2015-02-26 19:22:17 +0300

    ads

--- core/src/org/gamerex/buildthelines2/states/ActionResolver.java
+++
@@ -1,6 +0,0 @@
-package org.gamerex.buildthelines2.states;
-
-public interface ActionResolver {
-	public void showOrLoadInterstital();
-
-}
--- core/src/org/gamerex/buildthelines2/ui/Tile.java
+++
@@ -1,5 +0,0 @@
-package org.gamerex.buildthelines2.ui;
-
-public class Tile {
-
-}
